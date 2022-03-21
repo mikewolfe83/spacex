be717273d3d3b45129640e7a9127fd40e5308441
mikewolfe83 <mgray2@ualr.edu>
2022-03-21 20:22:58 +0000

    spacex update

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,3 +1,19 @@
 const scroll = new SmoothScroll('.navbar a[href*="#"]', {
     speed:800
 }); 
+
+$(document).ready(function() {
+
+    $('.fade').slick({
+      dots: true,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      slide: 'div',
+      cssEase: 'linear',
+      autoplay: true,
+      autoplaySpeed: 2000
+    });
+
+
+});
